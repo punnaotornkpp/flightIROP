9fae082fbe5a513c68e68226580a36018cc56866
Nokfly2024! <Punnatorn.Yim@nokair.co.th>
2025-03-24 09:37:23 +0700

    initial commit with apollo template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,80 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppLayout } from './layout/components/app.layout';
+import { Landing } from './pages/landing/landing';
+import { Notfound } from './pages/notfound/notfound';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppLayout,
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('../app/pages/dashboards/ecommercedashboard').then(
+            (c) => c.EcommerceDashboard
+          ),
+        data: { breadcrumb: 'E-Commerce Dashboard' },
+      },
+      {
+        path: 'dashboard-banking',
+        loadComponent: () =>
+          import('../app/pages/dashboards/bankingdashboard').then(
+            (c) => c.BankingDashboard
+          ),
+        data: { breadcrumb: 'Banking Dashboard' },
+      },
+      {
+        path: 'uikit',
+        data: { breadcrumb: 'UI Kit' },
+        loadChildren: () => import('../app/pages/uikit/uikit.routes'),
+      },
+      {
+        path: 'documentation',
+        data: { breadcrumb: 'Documentation' },
+        loadComponent: () =>
+          import('../app/pages/documentation/documentation').then(
+            (c) => c.Documentation
+          ),
+      },
+      {
+        path: 'pages',
+        loadChildren: () => import('../app/pages/pages.routes'),
+      },
+      {
+        path: 'apps',
+        loadChildren: () => import('../app/apps/apps.routes'),
+        data: { breadcrumb: 'Apps' },
+      },
+      {
+        path: 'blocks',
+        data: { breadcrumb: 'Free Blocks' },
+        loadChildren: () => import('../app/pages/blocks/blocks.routes'),
+      },
+      {
+        path: 'ecommerce',
+        loadChildren: () => import('../app/pages/ecommerce/ecommerce.routes'),
+        data: { breadcrumb: 'E-Commerce' },
+      },
+      {
+        path: 'profile',
+        loadChildren: () =>
+          import('../app/pages/usermanagement/usermanagement.routes'),
+      },
+    ],
+  },
+  { path: 'landing', component: Landing },
+  { path: 'notfound', component: Notfound },
+  {
+    path: 'auth',
+    loadChildren: () => import('../app/pages/auth/auth.routes'),
+  },
+  { path: '**', redirectTo: '/notfound' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import Aura from '@primeng/themes/aura';
+import { AppRoutingModule, routes } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {
+  provideRouter,
+  withEnabledBlockingInitialNavigation,
+  withInMemoryScrolling,
+} from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { providePrimeNG } from 'primeng/config';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        anchorScrolling: 'enabled',
+        scrollPositionRestoration: 'enabled',
+      }),
+      withEnabledBlockingInitialNavigation()
+    ),
+    provideHttpClient(withFetch()),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } },
+    }),
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
